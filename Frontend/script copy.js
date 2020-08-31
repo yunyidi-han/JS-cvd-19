cadca5c4d39d397fdadb21a354b78b7c4ba696a5
Yunyidi Han <yunyidi.han@gmail.com>
2020-08-31 15:14:20 -0700

    working script and index

--- Frontend/script copy.js
+++ Frontend/script copy.js
@@ -1,181 +1,50 @@
-let stateButton = document.getElementById("CHANGE");
-let dayButton = document.getElementById("CHANGE");
-let heatMapButton = document.getElementById("CHANGE");
-let infoContainer = document.getElementById("CHANGE");
+const symptomsTab = document.getElementById("symptoms");
+const stopSpread = document.getElementById("stop-spread");
+const coughimage = document.getElementById("cough-image");
+const lightBreathImage = document.getElementById("light-breath-image");
+const lightFeverImage = document.getElementById("light-fever-image");
+const wearMaskImage = document.getElementById("wear-mask-image");
 
-addButton.addEventListener("click", () => {
-  let id = document.getElementById("movieID").value;
-  let title = document.getElementById("movieTitle").value;
-  let actor = document.getElementById("movieActor").value;
-  let year = document.getElementById("movieYear").value;
-  let poster = document.getElementById("moviePoster").value;
-
-  if (poster == undefined)
-    poster =
-      "https://betravingknows.com/wp-content/uploads/2017/06/video-movie-placeholder-image-grey.png";
-
-  axios
-    .post(
-      `http://localhost:3000/add?id=${id}&title=${title}&actor=${actor}&year=${year}&poster=${poster}`
-    )
-    .then((response) => {
-      console.log(response);
-      addCard([response.data]);
-    });
+symptomsTab.addEventListener("click", () => {
+  coughimage.style.display = "block";
+  lightBreathImage.style.display = "block";
+  lightFeverImage.style.display = "block";
+  wearMaskImage.style.display = "none";
 });
 
-searchButton.addEventListener("click", () => {
-  axios.get("http://localhost:3000/movies").then((response) => {
-    console.log(response);
-    addCard(response.data);
-  });
+stopSpread.addEventListener("click", () => {
+  wearMaskImage.style.display = "block";
+  coughimage.style.display = "none";
+  lightBreathImage.style.display = "none";
+  lightFeverImage.style.display = "none";
 });
 
-let addCard = (response) => {
-  movieContainer.innerHTML = "";
-  for (let i = 0; i < response.length; i++) {
-    // cardID++;
-    let { id } = response[i];
-    let title = response[i].title;
-    let actor = response[i].actor;
-    let year = response[i].year;
-    let poster = response[i].poster;
+/*This portion of code is for the US map */
+$("path, circle").hover(function (e) {
+  $("#info-box").css("display", "block");
+  $("#info-box").html($(this).data("info"));
+});
 
-    let card = document.createElement("div");
-    card.setAttribute("class", "card");
+$("path, circle").mouseleave(function (e) {
+  $("#info-box").css("display", "none");
+});
 
-    let img = document.createElement("img");
-    img.setAttribute("alt", "Image of: " + title);
-    if (poster != "") {
-      img.setAttribute("src", poster);
-    } else {
-      img.setAttribute(
-        "src",
-        "https://betravingknows.com/wp-content/uploads/2017/06/video-movie-placeholder-image-grey.png"
-      );
-    }
+$("path, circle").click((e) => {
+  let state = e.target.id; //gets the 2 letter state ID when clicking on the map
+});
 
-    img.setAttribute("class", "card-img-top");
+$(document)
+  .mousemove(function (e) {
+    $("#info-box").css("top", e.pageY - $("#info-box").height() - 30);
+    $("#info-box").css("left", e.pageX - $("#info-box").width() / 2);
+  })
+  .mouseover();
 
-    card.appendChild(img);
-
-    let cardBody = document.createElement("div");
-    cardBody.setAttribute("class", "card-body");
-    let header = document.createElement("h4");
-    header.setAttribute("class", "card-title");
-    header.innerHTML = id + ". " + title;
-    let header2 = document.createElement("h5");
-    header2.setAttribute("class", "card-subtitle mb-2 text-muted");
-    header2.innerHTML = year;
-    let cardPara = document.createElement("p");
-    cardPara.setAttribute("class", "card-text");
-    cardPara.innerHTML = actor;
-
-    cardBody.appendChild(header);
-    cardBody.appendChild(document.createElement("hr"));
-    cardBody.appendChild(header2);
-    cardBody.appendChild(cardPara);
-    card.appendChild(cardBody);
-
-    let editBtn = document.createElement("button");
-    editBtn.setAttribute("class", "btn btn-primary");
-    editBtn.setAttribute("type", "button");
-    editBtn.innerHTML = "Edit";
-    editBtn.addEventListener("click", editCard);
-
-    let delBtn = document.createElement("button");
-    delBtn.setAttribute("class", "btn btn-danger");
-    delBtn.setAttribute("type", "button");
-    // delBtn.setAttribute("id", card + cardID);
-    delBtn.innerHTML = "Delete";
-    delBtn.addEventListener("click", deleteCard);
-
-    card.appendChild(editBtn);
-    card.appendChild(delBtn);
-
-    movieContainer.appendChild(card);
-  }
-
-  document.getElementById("addMovie").reset();
-};
-
-// let validate = () => {
-//   let keyword = document.getElementById("titleSearch").value;
-//   if (keyword == "") {
-//     alert("Please enter a title!");
-//   } else {
-//     movieContainer.innerHTML = "";
-//     axios
-//       .get("http://omdbapi.com/?apikey=168a6a83&s=" + keyword)
-//       .then((resp) => {
-//         let response = resp.data.Search;
-//         if (response == undefined) {
-//           let noResult = document.createElement("h1");
-//           noResult.innerHTML = "No Search Results!";
-//           movieContainer.appendChild(noResult);
-//           document.getElementById("sumbitSearch").reset();
-//         } else {
-//           addCard(response);
-//         }
-//       });
-//   }
-// };
-
-let editCard = (e) => {
-  let card = e.target.parentElement;
-  let movID = prompt("Enter a new movie ID");
-  let movTitle = prompt("Enter a new movie title");
-  let movActor = prompt("Enter a new movie actor");
-  let movYear = prompt("Enter a new movie year");
-  let movUrl = prompt("Enter a new URL for the image");
-
-  let id = card.children[1].children[0].innerHTML.substring(0, 1);
-  let title = card.children[1].children[0].innerHTML.substring(
-    2,
-    card.children[1].children[0].innerHTML.length - 1
-  );
-  let actor = card.children[1].children[2].innerHTML;
-  let year = card.children[1].children[3].innerHTML;
-  let url = card.children[0].getAttribute("src");
-
-  if (movID.length > 0) {
-    id = movID;
-    card.children[1].children[0].innerHTML.substring(0, 1) = id;
-  }
-  if (movTitle.length > 0) {
-    title = movTitle;
-    card.children[1].children[0].innerHTML = movTitle;
-  }
-  if (movActor.length > 0) {
-    card.children[1].children[2].innerHTML = movActor;
-  }
-  if (movYear.length > 0) {
-    card.children[1].children[3].innerHTML = movieYear;
-  }
-  if (movUrl.length > 0) {
-    url = movUrl;
-    card.children[0].setAttribute("src", movUrl);
-  }
-
-  // axios.put(`http://localhost:3000/edit?id=${header}`).then((response) => {
-  //   console.log(response);
-  // }); too lazy to do this, but its easy
-};
-
-let deleteCard = (e) => {
-  let header = e.target.parentElement.children[1].children[0].innerHTML.substring(
-    0,
-    1
-  );
-
-  console.log(header);
-  axios.delete(`http://localhost:3000/delete?id=${header}`).then((response) => {
-    console.log(response);
+var ios = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+if (ios) {
+  $("a").on("click touchend", function () {
+    var link = $(this).attr("href");
+    window.open(link, "_blank");
+    return false;
   });
-  e.target.parentElement.remove();
-};
-
-// document.getElementsByClassName(btn).addEventListener("click", deleteCard);
-//where to put this?
-
-// searchButton.addEventListener("click", validate);
+}
--- Frontend/script.js
+++ Frontend/script.js
@@ -44,3 +44,189 @@ if (ios) {
     return false;
   });
 }
+
+// ~~~~~~~~~~~~~~~~~~~~ DIDI'S CODE ~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~ Varibles to change ~~~~~~~~~~~~~~~
+
+let searchButton = document.getElementById("search"); //button for the action lisenter
+// let timeButton = document.getElementById("CHANGE");
+// let heatMapButton = document.getElementById("CHANGE");
+let infoContainer = document.getElementById("display-head"); // element where all the search stuff will return to
+let cardContainer = document.getElementById("display-cards");
+
+let stateBox = document.getElementById("state"); //element with the state's entry
+let timeBoxs = document.getElementsByName("radio-button-time"); // element with the time frame's entry
+
+let searchValidateMap = (state) => {
+  searchValidate(state);
+};
+
+let searchValidateButton = () => {
+  let state = stateBox.value; //takes the value in the search box after you click search
+  searchValidate(state);
+};
+
+let searchValidate = (state) => {
+  let timeFrame = "bleh";
+  for (let i = 0; i < timeBoxs.length; i++) {
+    if (timeBoxs[i].checked) {
+      timeFrame = timeBoxs[i].value;
+      break;
+    }
+  }
+  infoContainer.innerHTML = ""; //clear the container
+  axios
+    .get("https://api.covidtracking.com/v1/states/info.json")
+    .then((response) => {
+      let stateIndex = "State not found!";
+      if (state.length != 2) {
+        stateIndex = response.data.findIndex((ele) => ele.name === state);
+      } else {
+        stateIndex = response.data.findIndex((ele) => ele.state === state);
+      }
+      //finds the index of the object that contains the state we're searching for
+      let stateInfo = response.data[stateIndex];
+      let stateLetters = stateInfo.state;
+
+      //notes = snippet about state
+      //maybe twitter as well?
+      let stateHeader = document.createElement("div");
+      let header = document.createElement("h1");
+      header.innerHTML = state;
+      stateHeader.appendChild(header);
+      let notes = document.createElement("p");
+      notes.innerHTML = stateInfo.notes;
+      stateHeader.appendChild(notes);
+      infoContainer.appendChild(stateHeader);
+      // some other stuff here
+
+      searchState(stateLetters, timeFrame);
+    });
+};
+
+let searchState = (letters, time) => {
+  axios
+    .get(`https://api.covidtracking.com/v1/states/${letters}/daily.json`)
+    .then((response) => {
+      console.log(response);
+      //standard axios promise
+
+      if (time == 0) {
+        addCard([response.data[0]]);
+      } // pass an ARRAY of just 1 element to create the initial info card
+
+      //pass different numbers depending on the timeframe
+      else if (time == 1) {
+        addCard(response.data.slice(0, 7));
+      } else if (time == 2) {
+        //addCard([response.data.splice()]);
+      }
+
+      //for current stat of day
+    });
+};
+
+let addCard = (data) => {
+  cardContainer.innerHTML = "";
+  for (let i = 0; i < data.length; i++) {
+    // cardID++;
+    let date = data[i].date.toString();
+    date = new Date(
+      `${date.slice(4, 6)}/${date.slice(6, 8)}/${date.slice(0, 4)}`
+    ); // change to date format
+    let { hospitalizedCurrently } = data[i];
+    let { hospitalizedCumulative } = data[i];
+    let { onVentilatorCurrently } = data[i];
+    let { onVentilatorCumulative } = data[i];
+    let { positive } = data[i];
+    let { negative } = data[i];
+    let { totalTestResults } = data[i];
+    let { positiveIncrease } = data[i];
+    let { negativeIncrease } = data[i];
+    let { deathIncrease } = data[i];
+    let { deathConfirmed } = data[i];
+
+    let card = document.createElement("div");
+    card.setAttribute("class", "card");
+    card.style.maxWidth = "20%";
+
+    // let img = document.createElement("img");
+    // img.setAttribute("alt", "Image of: " + title);
+    // if (poster != "") {
+    //   img.setAttribute("src", poster);
+    // } else {
+    //   img.setAttribute(
+    //     "src",
+    //     "https://betravingknows.com/wp-content/uploads/2017/06/video-movie-placeholder-image-grey.png"
+    //   );
+    // }
+
+    // img.setAttribute("class", "card-img-top");
+
+    // card.appendChild(img);
+
+    let cardBody = document.createElement("div");
+    cardBody.setAttribute("class", "card-body");
+    let header = document.createElement("h4");
+    header.setAttribute("class", "card-title");
+    header.style.textAlign = "center";
+    header.innerHTML = date.toDateString();
+    // let header2 = document.createElement("h5");
+    // header2.setAttribute("class", "card-subtitle mb-2 text-muted");
+    // header2.innerHTML = year;
+    let cardPara = document.createElement("p");
+    cardPara.setAttribute("class", "card-text");
+    cardPara.innerHTML = `Hospitalized Currently: ${hospitalizedCurrently} <br>
+    Hospitalized Total: ${hospitalizedCumulative} <br> <hr> 
+    On Ventilators Currently: ${onVentilatorCurrently} <br>
+    On Ventilators Total: ${onVentilatorCumulative} <br> <hr> 
+    Tested Positve Total: ${positive} <br>
+    Tested Negative Total: ${negative} <br>
+    Tested Total: ${totalTestResults} <hr>
+    Tested Positive since the Day Before: ${positiveIncrease} <br>
+    Tested Negative since the Day Before: ${negativeIncrease} <br> <hr> 
+    Deaths since the day before: ${deathIncrease} <br>
+    Deaths Total: ${deathConfirmed}
+    `;
+
+    cardBody.appendChild(header);
+    cardBody.appendChild(document.createElement("hr"));
+    // cardBody.appendChild(header2);
+    cardBody.appendChild(cardPara);
+    card.appendChild(cardBody);
+
+    let editBtn = document.createElement("button");
+    editBtn.setAttribute("class", "btn btn-primary");
+    editBtn.setAttribute("type", "button");
+    editBtn.innerHTML = "Edit";
+    editBtn.addEventListener("click", editCard);
+
+    let delBtn = document.createElement("button");
+    delBtn.setAttribute("class", "btn btn-danger");
+    delBtn.setAttribute("type", "button");
+    delBtn.innerHTML = "Delete";
+    delBtn.addEventListener("click", deleteCard);
+
+    card.appendChild(editBtn);
+    card.appendChild(delBtn);
+
+    cardContainer.appendChild(card);
+  }
+
+  // document.getElementById("GImmie a form id man").reset();
+};
+
+let editCard = (e) => {
+  //maybe do something like ask for date later
+};
+
+let deleteCard = (e) => {
+  e.target.parentElement.remove();
+};
+
+searchButton.addEventListener("click", searchValidateButton);
+
+$("path, circle").click((e) => {
+  let state = e.target.id; //gets the 2 letter state ID when clicking on the map
+  searchValidateMap(state);
+});
